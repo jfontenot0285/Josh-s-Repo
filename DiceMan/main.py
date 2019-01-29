3225e730547ed6a502374071c55f881e6ccd277d
Josh <j.fontenot0285@gmail.com>
2019-01-29 12:37:28 -0800

    added shooting mechanic, cleaned up character creation

--- DiceMan/main.py
+++ DiceMan/main.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 from modules import *
-import os
 
 def main():
 
-    functions.set_stats()    
-    p1 = functions.char_select(1)
-    p2 = functions.char_select(2)
+    p1 = functions.set_stats(1)
+    p2 = functions.set_stats(2)
 
     print("*"*24)
     print("* Roll for first turn! *")
@@ -16,28 +14,9 @@ def main():
     rolls = functions.roll_off()
 
     if (rolls[0] > rolls[1]):
-        print("Player 2, would you like to seize the initiative?", \
-                "1 for yes; 2 for no: ")
-        seize_it = int(input())
-        os.system('clear')
-
-        if(seize_it == 2):
-            turn = "p1"
-
-        else:
-            turn = functions.seize_initiative(2)
-
+        turn = 1
     else:
-        print("Player 1, would you like to seize the initiative?", \
-                "1 for yes; 2 for no: ")
+        turn = 2
 
-        seize_it = int(input())
-        os.system('clear')
-
-        if(seize_it == 2):
-            turn = "p2"
-
-        else:
-            turn = functions.seize_initiative(1)
-
+    functions.shoot(p1[0])
 main()
--- DiceMan/modules/functions.py
+++ DiceMan/modules/functions.py
@@ -1,7 +1,7 @@
 from modules import statsclass, weaponstatsclass
-import random, os
+import random, os, platform
 
-def set_stats():
+def set_stats(p):
     #characters
     Warrior = statsclass.Stats(4,3,4,4,5,1,3) #(Sh, Me, St, Vi, Hp, At, Ar)
     Ranger = statsclass.Stats(3,4,3,3,4,1,4)
@@ -16,25 +16,41 @@ def set_stats():
     bayonet = weaponstatsclass.Weaponstats(0,1,1,0)
     knife = weaponstatsclass.Weaponstats(1,0,1,1)
     hammer = weaponstatsclass.Weaponstats(0,2,1,0)
+    
+    valid = 'n'
+    while(valid == 'n'):
+        print("\n","*"*32, sep='')
+        print("Player ",p," select your character:")
+        print("*"*32)
+        
+        print(" \t1) Space Marine\n",\
+              "\t2) Aeldari\n",\
+                "\t3) Ork\n")
+
+        sel = int(input())
+
+        if (sel == 1):
+            sel = Warrior, rifle, bayonet
+            valid = 'y'
+
+        elif(sel == 2):
+            sel = Ranger, sniper, knife
+            valid = 'y'
+
+        elif(sel == 3):
+            sel = Tanker, shotgun, hammer
+            valid = 'y'
+
+        else:
+            print('Enter a valid selection 1, 2, or 3:')
+
+    clear_screen()
+    return sel 
 
 def roll_dice():
     x = random.randint(1,6)
     return x
 
-def char_select(p):
-    print("\n","*"*32, sep='')
-    print("Player ",p," select your character:")
-    print("*"*32)
-
-    print(" \t1) Space Marine\n",\
-          "\t2) Aeldari\n",\
-          "\t3) Ork\n")
-
-    sel = int(input())
-    os.system('clear')
-
-    return sel
- 
 def roll_off():
     print("\nPlayer 1, press enter to roll:")
     input()
@@ -50,7 +66,7 @@ def roll_off():
 
     print("\nPress enter to continue")
     input()
-    os.system('clear')
+    clear_screen()
 
     if(p1_roll == p2_roll):
         print("Rolls are tied! Roll again.")
@@ -58,25 +74,23 @@ def roll_off():
 
     return p1_roll, p2_roll
 
-def seize_initiative(player):
-    print("*"*37)
-    print("\tSeize the initiative!")
-    print("*"*37,'\n')
-    print("Player", player, "will attempt to seize the initiative with a roll of 6!", \
-            "Player", player, "press enter to roll: ")
+def shoot(player):
+    print('Player fires upon opponent. Roll to see if you hit!')
+    print('Press enter to roll:')
     input()
+    clear_screen()
+    shoot_roll = roll_dice()
+    if (shoot_roll >= player.get_Sh()):
+        print('Player successfully hits opponent!')
+    else:
+        print('Player missed!')
+    print('Press enter to continue:')
+    input()
+    clear_screen()
 
-    player_roll = roll_dice()
-
-    print("Player", player, " you rolled a ", player_roll, "!\n",sep='')
-
-    if (player_roll == 6):
-        print("Player", player, "you have seized the initiative! Take the first turn.")
-        turn = player
+def clear_screen():
+    if (platform.system() == 'Windows'):
+        os.system('cls')
 
     else:
-        print("Player", player, "you have failed to seize the initiative. Opponent takes",\
-                "the first turn.")
-        turn = "op"
-
-    return turn
+        os.system('clear')
